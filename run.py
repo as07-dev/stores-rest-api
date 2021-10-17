c47ea2d5f6e3676cd5606dc2687b095ff775c850
Andrej Schneider <andrej.schneider@gmail.com>
2021-10-17 20:42:58 +0200

    postgres entfernt

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 from app import app
 from db import db
-from sqlalchemy_utils.functions import database_exists
+# from sqlalchemy_utils.functions import database_exists
 
 db.init_app(app)
 
 # Erstellung von Datenbank
 @app.before_first_request
 def create_tables():
-    if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
-        with sqlalchemy.create_engine(
-            'postgresql:///postgres',
-                isolation_level='AUTOCOMMIT').connect() as connection:
-            connection.execute('CREATE DATABASE data')
+    # if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+    #     with sqlalchemy.create_engine(
+    #         'postgresql:///postgres',
+    #             isolation_level='AUTOCOMMIT').connect() as connection:
+    #         connection.execute('CREATE DATABASE data')
     db.create_all()
